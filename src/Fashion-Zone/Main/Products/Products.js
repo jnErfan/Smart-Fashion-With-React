5aebb3d52fdecd23e0e407e40ab4313dfb5e4c71
jnErfan <j.n.erfan420@gmail.com>
2021-09-24 19:32:31 +0600

    Dynamic Rating

--- src/Fashion-Zone/Main/Products/Products.js
+++ src/Fashion-Zone/Main/Products/Products.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Rating from 'react-rating';
 import "./Products.css"
 
 const Products = (props) => {
@@ -13,15 +14,16 @@ const Products = (props) => {
         <h5>{title.slice(0,25)}</h5>
         <p>Category: {category}</p>
         <h2>Price : ${price}</h2>
-        <p>
-              <i className="fas fa-star text-warning"></i>
-              <i className="fas fa-star text-warning"></i>
-              <i className="fas fa-star text-warning"></i>
-              <i className="fas fa-star text-warning"></i>
-              <i className="far fa-star text-warning"></i>
-              <span className="rating">{rating.rate}</span>
-      </p>
-      <p><span className="fw-bold">{rating.count}</span> Person Rated</p>
+
+        <Rating
+        initialRating={rating.rate}
+        readonly
+        emptySymbol="far fa-star text-warning"
+        fullSymbol="fas fa-star text-warning"
+        />
+        <span className="rating">{rating.rate}</span>
+
+      <p className="mt-3"><span className="fw-bold">{rating.count}</span> Person Rated</p>
       <button className="btn btn-info text-white">ADD TO CART</button>
       <button className="btn btn-outline-info ms-2">DETAILS</button>
 
